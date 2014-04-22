980861d72d11de2dea59a241bc8c85a32c445ecb
Harun <harun.bulutlar@gmail.com>
2014-04-22 15:41:25 +0300

    Initial checkin

---
+++ TfsAccSwitch/TfsAccSwitch/Guids.cs
@@ -0,0 +1,21 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace NoComp.TfsAccSwitch
+{
+    static class GuidList
+    {
+        public const string guidTfsAccSwitchPkgString = "e98f8fa6-98b4-479d-89c7-8b99e6fa5b23";
+        public const string guidTfsAccSwitchCmdSetString = "d269264a-f6f7-49c7-b684-e6a1b6440895";
+        public static readonly Guid guidTfsAccSwitchCmdSet = new Guid(guidTfsAccSwitchCmdSetString);
+
+
+        //public const string GuidTfsAccSwitchPkgString = "e98f8fa6-98b4-479d-89c7-8b99e6fa5b23";
+        //public const string GuidTfsAccSwitchCmdSetString = "d269264a-f6f7-49c7-b684-e6a1b6440895";
+        //public const string GuidTeamExplorerString = "23D49123-60AC-4D7E-939A-E01A4E176BEE";
+        
+
+        //public static readonly Guid GuidTfsAccSwitchCmdSet = new Guid(GuidTfsAccSwitchCmdSetString);
+    };
+}
---
+++ TfsAccSwitch/TfsAccSwitch/PkgCmdID.cs
@@ -0,0 +1,11 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+
+namespace NoComp.TfsAccSwitch
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidShowAccount = 0x200;
+        public const uint cmdidChangeAccount = 0x300;
+    };
+} 
